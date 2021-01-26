fffdcc2b26c2ae0acbea0ae2d9a111cffc13d3cc
Aliaksandr Ivanou <alexiva@yandex-team.ru>
2021-01-26 22:02:26 +0300

    Add options and arguments description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ const command = new Command();
 
 command
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 command.parse();
