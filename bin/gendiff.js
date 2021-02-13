bdcad84589c84b08ed18946a1a3aa8fac7023ca8
Aliaksandr Ivanou <alexiva@yandex-team.ru>
2021-02-13 15:43:09 +0300

    Edit help text

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ const command = new Command();
 
 command
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a difference. Files with JSON and YAML files are supported.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
