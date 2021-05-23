ce9cd1dc88a901c9f948b7760d81e0b789198149
Alex Ivanov <alex.ivanov@unity3d.com>
2021-05-24 00:06:58 +0300

    Refactor stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -5,7 +5,7 @@ const ADDED_LINE_PREFIX = '+';
 const DELETED_LINE_PREFIX = '-';
 const LINE_BREAK = '\n';
 
-const getObjectString = (data, depth) => {
+const stringifyObject = (data, depth) => {
   if (!_.isObject(data)) {
     return data;
   }
@@ -13,58 +13,48 @@ const getObjectString = (data, depth) => {
   const indent = INDENT_CHAR.repeat(4);
   const dataIndent = indent.repeat(depth);
   const bracketsIndent = indent.repeat(depth - 1);
-  const lines = Object.entries(data).map(([key, value]) => `${dataIndent}${key}: ${getObjectString(value, depth + 1)}`);
+  const lines = Object.entries(data).map(
+    ([key, value]) => `${dataIndent}${key}: ${stringifyObject(value, depth + 1)}`,
+  );
 
-  return [
-    '{',
-    ...lines,
-    `${bracketsIndent}}`,
-  ].join(LINE_BREAK);
+  return ['{', ...lines, `${bracketsIndent}}`].join(LINE_BREAK);
 };
 
-const getStylishLine = (indent, prefix, key, value) => `${indent}${prefix} ${key}: ${value}`;
+const stringifyValue = (value, depth) => (_.isObject(value) ? stringifyObject(value, depth + 1) : value);
 
-const getValueString = (value, depth) => (_.isObject(value)
-  ? getObjectString(value, depth + 1)
-  : value);
+const stringify = (indent, prefix, key, value, depth) => `${indent}${prefix} ${key}: ${stringifyValue(value, depth)}`;
 
-const formatStylish = (diffAST) => {
-  const getStylishOutput = (ast, depth) => {
-    const indent = (INDENT_CHAR.repeat(4 * depth - 2));
-    const bracketsIndent = (INDENT_CHAR.repeat(4 * (depth - 1)));
+const formatStylish = (diffAST, currKey = '', depth = 1) => {
+  const indent = INDENT_CHAR.repeat(4 * depth - 2);
+  const bracketsIndent = INDENT_CHAR.repeat(4 * (depth - 1));
+  const firstLine = depth > 1 ? `${bracketsIndent}${currKey}: {` : `${bracketsIndent}{`;
 
-    const lines = ast.flatMap(({
-      key, type, value, children, oldValue,
-    }) => {
-      if (children) {
-        return getStylishLine(indent, INDENT_CHAR, key, getStylishOutput(children, depth + 1));
-      }
+  const mapDiffNode = (diffNode) => {
+    const {
+      type, key, value, oldValue, children,
+    } = diffNode;
 
-      switch (type) {
-        case 'added':
-          return getStylishLine(indent, ADDED_LINE_PREFIX, key, getValueString(value, depth));
-        case 'removed':
-          return getStylishLine(indent, DELETED_LINE_PREFIX, key, getValueString(value, depth));
-        case 'updated':
-          return [
-            getStylishLine(indent, DELETED_LINE_PREFIX, key, getValueString(oldValue, depth)),
-            getStylishLine(indent, ADDED_LINE_PREFIX, key, getValueString(value, depth)),
-          ];
-        default:
-          return getStylishLine(indent, INDENT_CHAR, key, getValueString(value, depth));
-      }
-    });
-
-    const separator = lines.length === 0 ? '' : LINE_BREAK;
-
-    return [
-      '{',
-      ...lines,
-      `${bracketsIndent}}`,
-    ].join(separator);
+    switch (type) {
+      case 'nested':
+        return formatStylish(children, key, depth + 1);
+      case 'added':
+        return stringify(indent, ADDED_LINE_PREFIX, key, value, depth);
+      case 'removed':
+        return stringify(indent, DELETED_LINE_PREFIX, key, value, depth);
+      case 'updated':
+        return [
+          stringify(indent, DELETED_LINE_PREFIX, key, oldValue, depth),
+          stringify(indent, ADDED_LINE_PREFIX, key, value, depth),
+        ];
+      default:
+        return stringify(indent, INDENT_CHAR, key, value, depth);
+    }
   };
 
-  return getStylishOutput(diffAST, 1);
+  const lines = diffAST.flatMap((diffNode) => mapDiffNode(diffNode));
+  const separator = lines.length === 0 ? '' : '\n';
+
+  return [firstLine, ...lines, `${bracketsIndent}}`].join(separator);
 };
 
-export default formatStylish;
+export default (diffAST) => formatStylish(diffAST);
