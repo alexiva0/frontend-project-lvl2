06f7d4199bdfefab17bd835a30ef9aa1c90cb743
Alex Ivanov <alex.ivanov@unity3d.com>
2021-08-13 14:48:23 +0300

    Test commit

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import buildDiffAST from './src/build-diff-ast.js';
 import formatOutput from './src/formatters/index.js';
 
 const genDiff = (pathOne, pathTwo, format = 'stylish') => {
-  const fileOneContent = getFileContent(pathOne);
   const fileTwoContent = getFileContent(pathTwo);
+  const fileOneContent = getFileContent(pathOne);
 
   const diffAST = buildDiffAST(fileOneContent, fileTwoContent);
 
